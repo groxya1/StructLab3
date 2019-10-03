0f942d60bdbdf04aa739f24adc827f729bf984b3
AeldRise <landserdc@gmail.com>
2019-10-03 17:42:53 +0300

    Более строгие обозначения.

--- struct4/src/org/dud/Main.java
+++ struct4/src/org/dud/Main.java
@@ -1,18 +1,16 @@
 package org.dud;
 
-import java.io.Console;
-
 public class Main
 {
     public static void main(String[] args) {
-        UselessSet test = new UselessSet();
+        Set test = new Set();
         test.Add(1,3,4,6);
         System.out.println(test.toString());
         test.AddNumbersInRange(30,50);
         test.Remove(17);
         System.out.println(test.toString());
-        System.out.println("изи: " +test.GetOnlyPrimeNumbers());
-        System.out.println("сложна: " +test.GetOnlyCompositeNumbers());
+        System.out.println("Простые числа: " +test.GetOnlyPrimeNumbers());
+        System.out.println("Составные числа: " +test.GetOnlyCompositeNumbers());
     }
 }
 
